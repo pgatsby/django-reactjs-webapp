6abc23a4c1739ef7abcc6bae23d377c42a0a5ec5
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2023-12-28 23:52:29 -0800

    feat(models): add core models for products/orders

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product, ProductReview, Order, OrderItem, ShippingAddress
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ProductReview)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)

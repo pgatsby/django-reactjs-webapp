e26833679f915c9745afdaf276c55e1f6707f1c6
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2024-06-26 19:08:51 -0700

    feat(auth): login with email

--- Server/settings.py
+++ Server/settings.py
@@ -158,6 +158,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'api.auth.EmailBackend']
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
